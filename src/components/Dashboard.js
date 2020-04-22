c05be42788a1f457a612b6312e1642fca1cd16f5
aftiyah <abd2010ly@gmail.com>
2020-04-22 23:15:08 +0200

    tweet component UI and api integration done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Tweet from './Tweet'
 
 class Dashboard extends Component {
   render() {
@@ -10,7 +11,7 @@ class Dashboard extends Component {
         <ul className='dashboard-list'>
           {this.props.tweetIds.map((id) => (
             <li key={id}>
-              <div>TWEET ID: {id}</div>
+              <Tweet id={id} />
             </li>
           ))}
         </ul>
