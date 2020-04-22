66d4fbdbe9722aa0714883da9255238b586ba6bb
aftiyah <abd2010ly@gmail.com>
2020-04-22 23:08:45 +0200

    dashboard component setup done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading === true
+          ? <div>Loading...</div>
+          : <Dashboard />}
       </div>
     )
+  }
+}
+
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
   }
 }
 
